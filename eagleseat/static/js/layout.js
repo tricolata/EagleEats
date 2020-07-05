8201a0b5be2b99f28729a495742b936d02248e5d
jakergrossman <jake.r.grossman@gmail.com>
2020-07-04 23:39:12 -0500

    Implement navbar and page links

---
+++ eagleseat/static/js/layout.js
@@ -0,0 +1,36 @@
+window.addEventListener('load', setActiveTab);
+
+function setActiveTab() {
+    // get last segment of page path
+    let pageName = window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
+    console.log(pageName);
+    let activeTab = undefined;
+    switch (pageName) {
+        case 'deals': {
+            activeTab = document.querySelector('#deals-button');
+            break;
+        }
+
+        case 'menu': {
+            activeTab = document.querySelector('#menu-button');
+            break;
+        }
+
+        case 'login': {
+            activeTab = document.querySelector('#login-button');
+            break;
+        }
+
+        case 'signup': {
+            activeTab = document.querySelector('#signup-button');
+            break;
+        }
+
+        case 'cart': {
+            activeTab = document.querySelector('#cart-button');
+            break;
+        }
+    }
+
+    activeTab.classList.add('active');
+}
