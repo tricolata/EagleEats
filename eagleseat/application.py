fef27cf748aa918b3d1acf61ea09f776d619c039
yafetkubrom <yafetkubrom@my.unt.edu>
2020-07-08 18:08:38 -0500

    user creation, check repeat email, login, hash password

--- eagleseat/application.py
+++ eagleseat/application.py
@@ -1,10 +1,16 @@
 """ Flask is a microframework to create web applications within python """
 
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, flash
+from passlib.hash import sha256_crypt
 import sqlite3
 from sqlite3 import Error
 from classes import MenuItem
+from classes import User
+import random
+import os
 
+
+used_ids = []
 """ Create a database connection to SQLite database """
 def create_connection(db_name):
 	conn = None
@@ -17,12 +23,13 @@ def create_connection(db_name):
 	return conn
 """ Create a new user into the customers table """
 def create_user(conn, user):
-	sql = ''' INSERT INTO customers(name, user_id, email, password, phone, address) VALUES (?,?,?,?,?,?) '''
+	sql = ''' INSERT INTO customers(user_id, name, email, password, phone) VALUES (?,?,?,?,?) '''
 	cur = conn.cursor()
 	cur.execute(sql, user)
 	return cur.lastrowid	# lastrowid attributes the cursor object to get back generated id
 
 app = Flask(__name__)
+app.secret_key = os.urandom(12)
 
 """ route() tells flask what URL triggers this function """
 @app.route("/")
@@ -42,22 +49,47 @@ def register():
 		""" request.form.get() accesses to input from html file """
 
 		name = request.form.get("name")
-		username = request.form.get("user_id")
 		email = request.form.get("email")
-		password = request.form.get("password")
+		password = sha256_crypt.encrypt(request.form.get("password"))
 		phone = request.form.get("phone")
-		address = request.form.get("address")
+		# random() could give same number so this will check
+		r = random.randint(1,200000)
+		while r in used_ids:
+			r = random.randint(1,200000)
+		used_ids.append(r)
+		user_id = r
 
 		with conn:
-			customer = (name, username, email, password, phone, address)
-			create_user(conn, customer)
+			sql = ''' SELECT * FROM customers where email = ? '''
+			cur = conn.cursor()
+			cur.execute(sql, (email, ))
+			if cur.fetchone():
+				flash("That email already exist, please log in or choose another")
+				return redirect(url_for('register'))
+			else:
+				customer = (user_id, name, email, password, phone)
+				create_user(conn, customer)
+				return render_template("index.html")
 
-		return render_template("index.html")
-
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-	return render_template("login.html")
+	if request.method == "POST":
+		database = "restaurant.db"
+		conn = create_connection(database)
 
+		email = request.form.get("email")
+		password = request.form.get("password")
+
+		sql = ''' SELECT password FROM customers WHERE email = ? '''
+		cur = conn.cursor()
+		cur.execute(sql, (email, ))
+		if not sha256_crypt.verify(password, cur.fetchone()[0]):
+			flash("Wrong email and/or password. Try again")
+			return redirect(url_for('login'))
+		else:
+			return render_template("index.html")
+	else:
+		return render_template("login.html")
 @app.route("/deals")
 def deals():
 	# TODO: get deals
