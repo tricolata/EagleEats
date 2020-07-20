6dd218a0575edfdb2e619e059e881fe17f4e2e85
jakergrossman <jake.r.grossman@gmail.com>
2020-07-19 23:46:32 -0500

    Implement account editing backend

--- eagleseat/static/js/account.js
+++ eagleseat/static/js/account.js
@@ -1,27 +1,53 @@
+// set eventlisteners for input fields
+for (input of document.querySelectorAll('input')) {
+    if (input.type == 'password') {
+        input.addEventListener('input', updateRequired);
+    } else {
+        input.addEventListener('input', updateSubmittable);
+    }
+}
+
 // set event listeners for password fields
 for (input of document.querySelectorAll('input[type="password"]')) {
     input.addEventListener('input', updateRequired);
 }
 
 function updateRequired() {
-    password_inputs = document.querySelectorAll('input[type="password"]')
+    passwordInputs = document.querySelectorAll('input[type="password"]');
 
-    // for each password field
-    for (input of password_inputs) {
-        input.required = true;
-        // if any input is filled, they are all required
-        if (input.value.length > 0) {
-            for (i of password_inputs) {
-                i.required = true;
+    // set password field as required if they have data
+    if (isPasswordRequired()) {
+        // for each password field
+        for (input of passwordInputs) {
+            if (input.value.length > 0) {
+                for (i of passwordInputs) {
+                    i.required = true;
+                }
             }
+        }
+    } else {
+        // if we've reached here, none of the password fields are filled
+        // so, set none of them required
+        for (input of passwordInputs) {
+            input.required = false;
+        }
+    }
+}
 
-            return;
+// returns true if any of the password fields are filled
+function isPasswordRequired() {
+    for (input of document.querySelectorAll('input[type="password"]')) {
+        if (input.value.length > 0) {
+            return true;
         }
     }
 
-    // if we've reached here, none of the password fields are filled
-    // so, set none of them required
-    for (input of password_inputs) {
-        input.required = false;
-    }
+    return false;
+}
+
+function hasMatchingPasswords() {
+    let newPassword = document.querySelector('#new-password');
+    let confirmPassword = document.querySelector('#confirm-password');
+
+    return newPassword === oldPassword;
 }
